dc360f74ce54649ddc6c86ed4fd27a33cb708a97
Ragnazar <avdeyev.iv@gmail.com>
2022-09-06 12:05:33 +0300

    add-emloyee-controller-basic

--- src/main/java/pro/sky/homeworks/homework31/exceptions/ArrayIsFull.java
+++ src/main/java/pro/sky/homeworks/homework31/exceptions/ArrayIsFull.java
@@ -1,4 +1,4 @@
-package pro.sky.homeworks.homework27.exceptions;
+package pro.sky.homeworks.homework31.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
